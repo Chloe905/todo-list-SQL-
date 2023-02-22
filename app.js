625e75a83e19fd6f427eddac6faf2b29359739ef
Chloe905 <cchloehsieh@gmail.com>
2023-02-22 21:37:29 +0800

    feat:add flash messages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const usePassport = require('./config/passport')
 const routes = require('./routes')
@@ -20,12 +21,13 @@ app.use(methodOverride('_method'))
 
 // 必須要在路由前
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  // res.locals.success_msg = req.flash('success_msg')
-  // res.locals.warning_msg = req.flash('warning_msg')
-  // res.locals.error = req.flash('error')
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error = req.flash('error')
   next()
 })
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -11,7 +11,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
@@ -20,30 +21,42 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = [] // 錯誤訊息
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位皆為必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不符' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors, name, email, password, confirmPassword
+    })
+  }
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
+      errors.push({ message: '此 Email 已註冊過!' })
+      return res.render('register', { errors, name, email, password, confirmPassword })
     }
     return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
+      .genSalt(10) // 產生鹽，設定複雜係數10
+      .then(salt => bcrypt.hash(password, salt)) // 密碼加鹽，產生雜湊
+      .then(hash => User.create({ // 尚未註冊:寫入資料庫
         name,
         email,
-        password: hash
+        password: hash // 雜湊值代替密碼
       }))
       .then(() => res.redirect('/'))
       .catch(err => console.log(err))
   })
 })
 
-router.get('/logout', (req, res) => {
-  res.send('logout')
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) { return next(err) }
+  })
+  req.flash('success_msg', '您以成功登出!')
+  res.redirect('/users/login')
 })
+
 module.exports = router
