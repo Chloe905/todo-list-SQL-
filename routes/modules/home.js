028b973bed8c3f18d1e4e2dd36f69ec26268dd66
Chloe905 <cchloehsieh@gmail.com>
2023-02-22 21:47:16 +0800

    add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,18 +5,24 @@ const router = express.Router()
 // 引用 Todo models
 const db = require('../../models')
 const Todo = db.Todo
-
+const User = db.User
 // 定義首頁路由
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => {
-      return res.render('index', { todos })
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
+
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: req.user.id }
+      })
+        .then((todos) => {
+          return res.render('index', { todos })
+        })
+        .catch((error) => { return res.status(422).json(error) })
     })
-    .catch((error) => { return res.status(422).json(error) })
 })
 
 // 匯出首頁路由模組
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -9,8 +9,9 @@ const Todo = db.Todo
 // 定義/todos路由
 
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const { id } = req.params
-  return Todo.findByPk(id)
+  return Todo.findOne({ where: { id, UserId } })
     .then(todo =>
       res.render('detail', { todo: todo.toJSON() })
     )
